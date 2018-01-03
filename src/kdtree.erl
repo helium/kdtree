0b8bcf229a45f28bb50bcc0ba405b64e2db72eaf
Rahul Garg <rahul@helium.com>
2018-01-03 15:50:10 -0800

    allow adding value to a node, add basic test

--- src/kdtree.erl
+++ src/kdtree.erl
@@ -1,9 +1,12 @@
 -module(kdtree).
 
+-type coordinate() :: {float(), float()}.
+
 -record(node, {
-          location,
-          left,
-          right
+          location :: coordinate(),
+          value :: any(),
+          left :: #node{} | undefined,
+          right :: #node{} | undefined
          }).
 
 % greater than max distance between two points on earth
@@ -11,22 +14,23 @@
 % in miles
 -define(EARTHRADIUS, 3961).
 
--export([build/1, nearby/3, nearest/2]).
+-export([from_list/1, haversine_distance/2, nearby/3, nearest/2, to_list/1]).
 
--spec build(list()) -> #node{}.
-build(CoordinateList) ->
-    build(CoordinateList, 0).
+-spec from_list([{coordinate(), any()}, ...]) -> #node{}.
+from_list(CoordinateList) when length(CoordinateList) > 0 ->
+    from_list(CoordinateList, 0).
 
--spec build(list(), non_neg_integer()) -> undefined | #node{}.
-build(CoordinateList, _Depth) when length(CoordinateList) == 0 ->
+-spec from_list([{coordinate(), any()}], non_neg_integer()) -> #node{}.
+from_list([], _Depth) ->
     undefined;
-build(CoordinateList, Depth) when length(CoordinateList) == 1 ->
+from_list([{Coordinate, Value}], _Depth) ->
     #node{
-       location=hd(CoordinateList),
-       left=build([], Depth + 1),
-       right=build([], Depth + 1)
+       location=Coordinate,
+       value=Value,
+       left=undefined,
+       right=undefined
       };
-build(CoordinateList, Depth) ->
+from_list(CoordinateList, Depth) ->
     K = tuple_size(lists:nth(1, CoordinateList)),
     Axis = Depth rem K,
     SortedCoordinateList = case Axis of
@@ -34,27 +38,25 @@ build(CoordinateList, Depth) ->
                                1 -> lists:keysort(2, CoordinateList)
                            end,
     Median = (length(SortedCoordinateList) div 2) + 1,
+    {Coordinate, Value} = lists:nth(Median, SortedCoordinateList),
     #node{
-       location= lists:nth(Median, SortedCoordinateList),
-       left=build(lists:sublist(SortedCoordinateList, 1, Median - 1), Depth + 1),
-       right=build(lists:sublist(SortedCoordinateList, Median + 1, length(SortedCoordinateList)), Depth + 1)
+       location=Coordinate,
+       value=Value,
+       left=from_list(lists:sublist(SortedCoordinateList, 1, Median - 1), Depth + 1),
+       right=from_list(lists:sublist(SortedCoordinateList, Median + 1, length(SortedCoordinateList)), Depth + 1)
        }.
 
--spec nearest(#node{} | undefined, tuple()) -> undefined | tuple().
-nearest(undefined, _Coordinate) ->
-    undefined;
+-spec nearest(#node{}, coordinate()) -> {coordinate(), float()}.
 nearest(Node, Coordinate) ->
     nearest(Node, Coordinate, Node#node.location, ?MAXDISTANCE, 0).
 
--spec nearest(#node{}, tuple(), tuple(), non_neg_integer(), non_neg_integer()) -> undefined | tuple().
-nearest(undefined, _Coordinate, Closest, MinDist, _Depth) ->
-    {Closest, MinDist};
+-spec nearest(#node{}, tuple(), tuple(), non_neg_integer(), non_neg_integer()) -> tuple().
 nearest(Node, Coordinate, Closest, MinDist, Depth) ->
     Axis = get_axis(Depth, Coordinate),
     Distance = haversine_distance(Coordinate, Node#node.location),
     {NewClosest, NewMinDist} = case Distance < MinDist andalso Coordinate /= Node#node.location of
                                    true ->
-                                       {Node#node.location, Distance};
+                                       {{Node#node.location, Node#node.value}, Distance};
                                    false ->
                                        {Closest, MinDist}
                                end,
@@ -67,7 +69,7 @@ nearest(Node, Coordinate, Closest, MinDist, Depth) ->
             nearest(Right, Coordinate, NewClosest, NewMinDist, Depth + 1);
         {true, Left, Right} ->
             {NewerClosest, NewerMinDist} = nearest(Left, Coordinate, NewClosest, NewMinDist, Depth + 1),
-            case (PointDim + MinDist) >= NodeDim of
+            case (PointDim + MinDist) >= NodeDim andalso Right /= undefined of
                 true -> nearest(Right, Coordinate, NewerClosest, NewerMinDist, Depth + 1);
                 false -> {NewerClosest, NewerMinDist}
             end;
@@ -75,26 +77,24 @@ nearest(Node, Coordinate, Closest, MinDist, Depth) ->
             nearest(Left, Coordinate, NewClosest, NewMinDist, Depth + 1);
         {false, Left, Right} ->
             {NewerClosest, NewerMinDist} = nearest(Right, Coordinate, NewClosest, NewMinDist, Depth + 1),
-            case (PointDim - MinDist) =< NodeDim of
+            case (PointDim - MinDist) =< NodeDim andalso Left /= undefined of
                 true -> nearest(Left, Coordinate, NewClosest, NewMinDist, Depth + 1);
                 false -> {NewerClosest, NewerMinDist}
             end
     end.
 
--spec nearby(#node{} | undefined, tuple(), tuple()) -> undefined | list().
-nearby(undefined, _Coordinate, _Range) ->
-    undefined;
+-spec nearby(#node{}, coordinate(), pos_integer()) -> list().
 nearby(Node, Coordinate, Range) ->
     nearby(Node, Coordinate, Node#node.location, Range, 0, []).
 
--spec nearby(#node{} | undefined, tuple(), tuple(), non_neg_integer(), non_neg_integer(), list()) -> undefined | list().
+-spec nearby(#node{} | undefined, coordinate(), coordinate(), pos_integer(), non_neg_integer(), list()) -> list().
 nearby(undefined, _Coordinate, _NearbyCoordinate, _Range, _Depth, List) ->
     List;
 nearby(Node, Coordinate, NearbyCoordinate, Range, Depth, List) ->
     Distance = haversine_distance(Coordinate, Node#node.location),
     NewList = case Distance < Range andalso Coordinate /= Node#node.location of
                   true ->
-                      List ++ [Node#node.location];
+                      [{Node#node.location, Node#node.value} | List];
                   false ->
                       List
               end,
@@ -102,11 +102,11 @@ nearby(Node, Coordinate, NearbyCoordinate, Range, Depth, List) ->
     nearby(Node#node.left, Coordinate, NearbyCoordinate, Range, Depth + 1, NewList) ++
         (nearby(Node#node.right, Coordinate, NearbyCoordinate, Range, Depth + 1, NewList) -- NewList).
 
--spec get_dimension(1 | 2, tuple()) -> float().
+-spec get_dimension(1 | 2, coordinate()) -> float().
 get_dimension(Axis, Coordinate) ->
     lists:nth(Axis, tuple_to_list(Coordinate)).
 
--spec haversine_distance(tuple(), tuple()) -> float().
+-spec haversine_distance(coordinate(), coordinate()) -> float().
 haversine_distance({Lat1, Long1}, {Lat2, Long2}) ->
     V = math:pi()/180,
     DeltaLat = (Lat2 - Lat1) * V,
@@ -115,9 +115,13 @@ haversine_distance({Lat1, Long1}, {Lat2, Long2}) ->
     C = 2 * math:atan2(math:sqrt(A), math:sqrt(1-A)),
     ?EARTHRADIUS * C.
 
--spec get_axis(non_neg_integer(), tuple()) -> 1 | 2.
+-spec get_axis(non_neg_integer(), coordinate()) -> 1 | 2.
 get_axis(Depth, Coordinate) ->
     case Depth rem tuple_size(Coordinate) of
         0 -> 1;
         1 -> 2
     end.
+
+-spec to_list(#node{}) -> [{coordinate(), any()}, ...].
+to_list(Node) ->
+    [{Node#node.location, Node#node.value} | nearby(Node, Node#node.location, ?MAXDISTANCE)].
---
+++ test/kdtree_test.erl
@@ -0,0 +1,33 @@
+-module(kdtree_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+basic_test() ->
+    % random list of GPS coordinates
+    List = [{{48.55321,-125.02235}, make_ref()},
+            {{45.26664,-66.06525}, make_ref()},
+            {{51.64982,-121.28594}, make_ref()},
+            {{51.6675,-121.29361}, make_ref()},
+            {{47.811,-53.97733}, make_ref()},
+            {{51.685,-121.29861}, make_ref()},
+            {{63.89335,-139.1991}, make_ref()},
+            {{51.84983,-121.60273}, make_ref()}],
+    % build a kdtree
+    Tree = kdtree:from_list(List),
+    % Verify kdtree root node is not undefined
+    ?assertNotEqual(undefined, Tree),
+    % Some Random Coordinate from list
+    % RandCoordinate = {51.84983,-121.60273},
+    {RandCoordinate, _} = lists:nth(rand:uniform(length(List)), List),
+    io:format("RandCoordinate: ~p", [RandCoordinate]),
+    % Verify nearest location from given location
+    {NearestLoc, Distance} = kdtree:nearest(Tree, RandCoordinate),
+    ?assertNotEqual(undefined, NearestLoc),
+    % Verify nearby
+    Nearby = kdtree:nearby(Tree, RandCoordinate, round(Distance) + 1),
+    % nearby list should not be empty
+    ?assertNotEqual([], Nearby),
+    % there should be only one coordinate in nearby list
+    ?assert(length(Nearby) >= 1),
+    % check identity
+    ?assertEqual(lists:usort(List), lists:usort(kdtree:to_list(Tree))).
